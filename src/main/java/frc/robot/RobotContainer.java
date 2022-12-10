19fb08347f947a62ba12f64974becf901fcfc5a2
Grig84 <44533697+Grig84@users.noreply.github.com>
2022-12-10 14:11:53 -0600

    Auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -98,14 +98,14 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return new SequentialCommandGroup(
+      new RunCommand(() -> {m_liftSubsystem.makeSpin(.5);}, m_liftSubsystem).withTimeout(.6),
 
-      //new ParallelCommandGroup(
-      //  new RunCommand(() -> m_shooter.armOut(), m_shooter),
-      //  new RunCommand(() -> {m_liftSubsystem.makeSpin(.5);}, m_liftSubsystem)
-      //).withTimeout(.25), 
+      new RunCommand(() -> driveSubsystem.drive(.55,.55), driveSubsystem).withTimeout(4),
+
+      new RunCommand(() -> m_shooter.armOut(), m_shooter).withTimeout(.25)
 
       //new RunCommand(() -> {driveSubsystem.drive(.55,-.55);}, driveSubsystem).withTimeout(.75),
-      new RunCommand(() -> {driveSubsystem.drive(.55,.25);}, driveSubsystem).withTimeout(2)
+      //new RunCommand(() -> {driveSubsystem.drive(.55,.25);}, driveSubsystem).withTimeout(2)
     );
   }
 }
